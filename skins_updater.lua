a2f2df47a521487c59d029ba3b84e20854771636
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2023-08-26 21:25:39 -0400

    replace skinsdb api source

--- skins_updater.lua
+++ skins_updater.lua
@@ -47,9 +47,9 @@ if #internal.errors > 0 then
 end
 
 -- http://minetest.fensta.bplaced.net/api/apidoku.md
-local root_url = "http://minetest.fensta.bplaced.net"
-local page_url = root_url .. "/api/v2/get.json.php?getlist&page=%i&outformat=base64" -- [1] = Page#
-local preview_url = root_url .. "/skins/1/%i.png" -- [1] = ID
+local root_url = "http://skinsdb.terraqueststudios.net"
+local page_url = root_url .. "/api/v1/content?test=test" -- [1] = Page#
+--local preview_url = root_url .. "/skins/1/%i.png" -- [1] = ID
 
 local mod_path = skins.modpath
 local meta_path = mod_path .. "/meta/"
@@ -100,9 +100,9 @@ local function safe_single_skin(skin)
 		skins_path .. name .. ".png",
 		core.decode_base64(skin.img)
 	)
-	fetch_url(preview_url:format(skin.id), function(preview)
+	--[[ fetch_url(preview_url:format(skin.id), function(preview)
 		unsafe_file_write(skins_path .. name .. skins.fsep .. "preview.png", preview)
-	end)
+	end) ]]
 	core.log("action", ("%s: Completed skin %s"):format(_ID_, name))
 end
 
